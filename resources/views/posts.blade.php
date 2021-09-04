e1c88977a7c20c9e253b4d2739cb55d3553620f6
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2021-09-04 06:55:02 +0100

    Finished single post view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,3 @@
-{{-- <x-layout>
-  @foreach ($posts as $post)
-    <article>
-      <h1>
-        <a href="/posts/{{ $post->slug }}">
-          {{ $post->title }}
-        </a>
-      </h1>
-      By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-      <div>
-      {{ $post->excerpt }}
-      </div>
-    </article>
-  @endforeach
-</x-layout> --}}
-
 <x-layout>
   @include('_posts-header')
 
